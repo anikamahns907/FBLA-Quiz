5ebafc94d1db949bd43fea2b55bc31e7dd22c43c
anikamahns907 <64509221+anikamahns907@users.noreply.github.com>
2021-03-25 13:37:12 -0400

    updated

--- frontend/src/Components/Question.js
+++ frontend/src/Components/Question.js
@@ -1,146 +1,148 @@
-import React from "react";
+// import React from "react";
 
-class Question extends React.Component {
-  constructor(props) {
-    super(props);
-    //states for collecting the User input and id of each question
-    this.state = {
-      id: "",
-      userAnswer: "Q: NOT ANSWERED",
-      userTfAnswer: "T/F: NOT ANSWERED",
-    };
-  }
+// class Question extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     //states for collecting the User input and id of each question
+//     this.state = {
+//       id: "",
+//       userAnswer: "Q: NOT ANSWERED",
+//       userTfAnswer: "T/F: NOT ANSWERED",
+//     };
+//   }
 
-  handleChangeOfDropDown(id, event) {
-    this.setState({ id: id, userAnswer: event.target.value });
-  }
+//   handleChangeOfDropDown(id, event) {
+//     this.setState({ id: id, userAnswer: event.target.value });
+//   }
 
-  handleWrittenResponse(id, event) {
-    this.setState({ id: id, userAnswer: event.target.value });
-  }
+//   handleWrittenResponse(id, event) {
+//     this.setState({ id: id, userAnswer: event.target.value });
+//   }
 
-  //function that uses if/else if statements to determine what state (either userTfAnswer or userAnswer) to pass the user input
-  handleStates(id, usersAnswer) {
-    if (usersAnswer === "FALSE" || usersAnswer === "TRUE") {
-      this.setState({ id: id, userTfAnswer: usersAnswer });
-    } else {
-      this.setState({ id: id, userAnswer: usersAnswer });
-    }
-  }
+//   //function that uses if/else if statements to determine what state (either userTfAnswer or userAnswer) to pass the user input
+//   handleStates(id, usersAnswer) {
+//     if (usersAnswer === "FALSE" || usersAnswer === "TRUE") {
+//       this.setState({ id: id, userTfAnswer: usersAnswer });
+//     } else {
+//       this.setState({ id: id, userAnswer: usersAnswer });
+//     }
+//   }
 
-  //function to determine how the question will be formatted (True or False, Multiple Choice, Drop Down, Written Response)
-  displayQuestion(type, data) {
-    if (type === "tf") {
-      return (
-        <div>
-          <h4>{data.tfversion}</h4>
-          <button onClick={() => this.handleStates(data._id, "TRUE")}>
-            true
-          </button>
-          <button onClick={() => this.handleStates(data._id, "FALSE")}>
-            false
-          </button>
-        </div>
-      );
-    } else if (type === "mc") {
-      return (
-        <div>
-          <h4>{data.question}</h4>
-          <form>
-            <input
-              type="radio"
-              name="answerChoice"
-              onClick={() => this.handleStates(data._id, data.c1)}
-              id={data.c1}
-              value={data.c1}
-            />
-            <label htmlFor={data.c1}>{data.c1}</label>
-            <br />
-            <input
-              type="radio"
-              name="answerChoice"
-              onClick={() => this.handleStates(data._id, data.c2)}
-              id={data.c2}
-              value={data.c2}
-            />
-            <label htmlFor={data.c2}>{data.c2}</label>
-            <br />
-            <input
-              type="radio"
-              name="answerChoice"
-              onClick={() => this.handleStates(data._id, data.c3)}
-              id={data.c3}
-              value={data.c3}
-            />
-            <label htmlFor={data.c3}>{data.c3}</label>
-            <br />
-            <input
-              type="radio"
-              name="answerChoice"
-              onClick={() => this.handleStates(data._id, data.c4)}
-              id={data.c4}
-              value={data.c4}
-            />
-            <label htmlFor={data.c4}>{data.c4}</label>
-          </form>
-        </div>
-      );
-    } else if (type === "dd") {
-      return (
-        <div>
-          <h4>{data.question}</h4>
-          <select
-            onChange={(event) => this.handleChangeOfDropDown(data._id, event)}
-          >
-            <option value={data.c1}>{data.c1}</option>
-            <option value={data.c2}>{data.c2}</option>
-            <option value={data.c3}>{data.c3}</option>
-            <option value={data.c4}>{data.c4}</option>
-          </select>
-        </div>
-      );
-    } else if (type === "wr") {
-      return (
-        <div>
-          <h4>{data.question}</h4>
-          <form>
-            <p className="typeBelow">Type your answer below.</p>
-            <label>
-              <input
-                type="text"
-                name="name"
-                onChange={(event) =>
-                  this.handleWrittenResponse(data._id, event)
-                }
-              />
-            </label>
-          </form>
-          {/* <form>
-            <label for="writtenResponse">Type your answer below</label>
-            <br />
-            <input type="text" id="writtenResponse" name="writtenResponse" />
-            <br />
-          </form> */}
-        </div>
-      );
-    }
-  }
+//   //function to determine how the question will be formatted (True or False, Multiple Choice, Drop Down, Written Response)
+//   displayQuestion(type, data) {
+//     if (type === "tf") {
+//       return (
+//         <div>
+//           <h4>{data.tfversion}</h4>
+//           <button onClick={() => this.handleStates(data._id, "TRUE")}>
+//             true
+//           </button>
+//           <button onClick={() => this.handleStates(data._id, "FALSE")}>
+//             false
+//           </button>
+//         </div>
+//       );
+//     } else if (type === "mc") {
+//       return (
+//         <div>
+//           <h4>{data.question}</h4>
+//           <form>
+//             <input
+//               type="radio"
+//               name="answerChoice"
+//               onClick={() => this.handleStates(data._id, data.c1)}
+//               id={data.c1}
+//               value={data.c1}
+//             />
+//             <label htmlFor={data.c1}>{data.c1}</label>
+//             <br />
+//             <input
+//               type="radio"
+//               name="answerChoice"
+//               onClick={() => this.handleStates(data._id, data.c2)}
+//               id={data.c2}
+//               value={data.c2}
+//             />
+//             <label htmlFor={data.c2}>{data.c2}</label>
+//             <br />
+//             <input
+//               type="radio"
+//               name="answerChoice"
+//               onClick={() => this.handleStates(data._id, data.c3)}
+//               id={data.c3}
+//               value={data.c3}
+//             />
+//             <label htmlFor={data.c3}>{data.c3}</label>
+//             <br />
+//             <input
+//               type="radio"
+//               name="answerChoice"
+//               onClick={() => this.handleStates(data._id, data.c4)}
+//               id={data.c4}
+//               value={data.c4}
+//             />
+//             <label htmlFor={data.c4}>{data.c4}</label>
+//           </form>
+//         </div>
+//       );
+//     } else if (type === "dd") {
+//       return (
+//         <div>
+//           <h4>{data.question}</h4>
+//           <select
+//             onChange={(event) => this.handleChangeOfDropDown(data._id, event)}
+//           >
+//             <option value={data.c1}>{data.c1}</option>
+//             <option value={data.c2}>{data.c2}</option>
+//             <option value={data.c3}>{data.c3}</option>
+//             <option value={data.c4}>{data.c4}</option>
+//           </select>
+//         </div>
+//       );
+//     } else if (type === "wr") {
+//       return (
+//         <div>
+//           <h4>{data.question}</h4>
+//           <form>
+//             <p className="typeBelow">Type your answer below.</p>
+//             <label>
+//               <input
+//                 type="text"
+//                 name="name"
+//                 onChange={(event) =>
+//                   this.handleWrittenResponse(data._id, event)
+//                 }
+//               />
+//             </label>
+//           </form>
+//           {/* <form>
+//             <label for="writtenResponse">Type your answer below</label>
+//             <br />
+//             <input type="text" id="writtenResponse" name="writtenResponse" />
+//             <br />
+//           </form> */}
+//         </div>
+//       );
+//     }
+//   }
 
-  render() {
-    var type = this.props.typeOfQuestion;
-    var data = this.props.questionData;
-    var theQuestion = this.displayQuestion(type, data);
-    return (
-      <div className="questions">
-        {theQuestion}
-        <p> {this.state.userTfAnswer} </p>
-        <p>{this.state.userAnswer}</p>
-        <br></br>
-        <br></br>
-        <hr></hr>
-      </div>
-    );
-  }
-}
+//   render() {
+//     var type = this.props.typeOfQuestion;
+//     var data = this.props.questionData;
+//     var theQuestion = this.displayQuestion(type, data);
+//     return (
+//       <div>
+//         <div className="questions">
+//           {theQuestion}
+//           <p> {this.state.userTfAnswer} </p>
+//           <p>{this.state.userAnswer}</p>
+//           <br></br>
+//           <br></br>
+//           <hr></hr>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 
-export default Question;
+// export default Question;
--- frontend/src/Components/Quiz.js
+++ frontend/src/Components/Quiz.js
@@ -1,6 +1,5 @@
 import React from "react";
 import axios from "axios";
-import Question from "./Question";
 
 class Quiz extends React.Component {
   constructor(props) {
@@ -15,80 +14,23 @@ class Quiz extends React.Component {
       ],
     };
   }
-  //before the get request:::
-  //what is the type of this.state.questions: An array of dictionaries
-  //this.state.questions[0]: {question: ""} type: dictionary
-  //this.state.questions[3]: { question: "" } type: dictionary
-  //this.state.questions[0].question: ""
-  //this.state.questions[2].question: ""
-  //this.state.questions[1].c1: null (if printed error crashed)
-  //this.state.questions[3].tfquestion: null (if printed error crashed)
 
-  //after a succesfull get request:::
-  //   this.state.questions is now equal to this
-  //   [
-  //     {
-  //         "_id": "6046e8834c79880b6cf56834",
-  //         "question": "How many administrative regions is FBLA-PBL divided into?",
-  //         "tfversion": "FBLA-PBL is divided into 2 regions.",
-  //         "c1": 5,
-  //         "c2": 2,
-  //         "c3": 8,
-  //         "c4": 4
-  //     },
-  //     {
-  //         "_id": "6046e8834c79880b6cf56826",
-  //         "question": "When is National FBLA-PBL Week?",
-  //         "tfversion": "National FBLA-PBL Week is held during February 14-20, 2021",
-  //         "c1": "March 4-11, 2021",
-  //         "c2": "February 14-20, 2021",
-  //         "c3": "August 21-28, 2021",
-  //         "c4": "November 20-27, 2021"
-  //     },
-  //     {
-  //         "_id": "6046e8834c79880b6cf56842",
-  //         "question": "How are ties in objective tests broken?",
-  //         "tfversion": "Ties in objective tests are broken by comparing the correct number of answers to the last 10 questions on the exam.",
-  //         "c1": "Ties in objective tests are broken by comparing the correct number of answers to the first 10 questions on the exam.",
-  //         "c2": "Ties in objective tests are broken by comparing the correct number of answers to the last question on the exam.",
-  //         "c3": "Ties in objective tests are broken by comparing the correct number of answers to the first question on the exam.",
-  //         "c4": "Ties are not broken; there will be two winners."
-  //     },
-  //     {
-  //         "_id": "6046e8834c79880b6cf56825",
-  //         "question": "How many chapters does the High School Division of FBLA-PBL obtain?",
-  //         "tfversion": "The High School Division of FBLA-PBL obtains around 1,570+ chapters?",
-  //         "c1": "around 5,200+ chapters",
-  //         "c2": "around 1,530+ chapters",
-  //         "c3": "around 1,570+ chapters",
-  //         "c4": "around 6,930+ chapters"
-  //     },
-  //     {
-  //         "_id": "6046e8834c79880b6cf56845",
-  //         "question": "How many times can a member of a team who has competed in the same event at one previous NLC compete in the same event at the NLC?",
-  //         "tfversion": "A competitor of a team who has competed in the same event at one previous NLC can compete in the event at the national level twice.",
-  //         "c1": "twice",
-  //         "c2": "three times",
-  //         "c3": "six times",
-  //         "c4": "only once"
-  //     }
-  // ]
+  handleChangeOfDropDown(id, event) {
+    this.setState({ id: id, userAnswer: event.target.value });
+  }
 
-  //what is the type of this.state.questions now?: Array of dictionaries
-  //this.state.questions[0]: type: dictionary
-  //     {
-  //         "_id": "6046e8834c79880b6cf56834",
-  //         "question": "How many administrative regions is FBLA-PBL divided into?",
-  //         "tfversion": "FBLA-PBL is divided into 2 regions.",
-  //         "c1": 5,
-  //         "c2": 2,
-  //         "c3": 8,
-  //         "c4": 4
-  //     }
-  //this.state.questions[0].question: "How many administrative regions is FBLA-PBL divided into?"
-  //this.state.questions[3].tfquestion: null (crash bc tfquestion not in dictionary)
-  //this.state.questions[3].tfversion:  "The High School Division of FBLA-PBL obtains around 1,570+ chapters?"
-  //this.state.questions[3].tfversionn: null (crash bc tfversionn not in dictionary)
+  handleWrittenResponse(id, event) {
+    this.setState({ id: id, userAnswer: event.target.value });
+  }
+
+  //function that uses if/else if statements to determine what state (either userTfAnswer or userAnswer) to pass the user input
+  handleStates(id, usersAnswer) {
+    if (usersAnswer === "FALSE" || usersAnswer === "TRUE") {
+      this.setState({ id: id, userTfAnswer: usersAnswer });
+    } else {
+      this.setState({ id: id, userAnswer: usersAnswer });
+    }
+  }
 
   getQuestion() {
     // Make a request for a user with a given ID
@@ -108,18 +50,148 @@ class Quiz extends React.Component {
       });
   }
 
+  displayQuestion(type, data) {
+    if (type === "tf") {
+      return (
+        <div>
+          <h4>{data.tfversion}</h4>
+          <button onClick={() => this.handleStates(data._id, "TRUE")}>
+            true
+          </button>
+          <button onClick={() => this.handleStates(data._id, "FALSE")}>
+            false
+          </button>
+        </div>
+      );
+    } else if (type === "mc") {
+      return (
+        <div>
+          <h4>{data.question}</h4>
+          <form>
+            <input
+              type="radio"
+              name="answerChoice"
+              onClick={() => this.handleStates(data._id, data.c1)}
+              id={data.c1}
+              value={data.c1}
+            />
+            <label htmlFor={data.c1}>{data.c1}</label>
+            <br />
+            <input
+              type="radio"
+              name="answerChoice"
+              onClick={() => this.handleStates(data._id, data.c2)}
+              id={data.c2}
+              value={data.c2}
+            />
+            <label htmlFor={data.c2}>{data.c2}</label>
+            <br />
+            <input
+              type="radio"
+              name="answerChoice"
+              onClick={() => this.handleStates(data._id, data.c3)}
+              id={data.c3}
+              value={data.c3}
+            />
+            <label htmlFor={data.c3}>{data.c3}</label>
+            <br />
+            <input
+              type="radio"
+              name="answerChoice"
+              onClick={() => this.handleStates(data._id, data.c4)}
+              id={data.c4}
+              value={data.c4}
+            />
+            <label htmlFor={data.c4}>{data.c4}</label>
+          </form>
+        </div>
+      );
+    } else if (type === "dd") {
+      return (
+        <div>
+          <h4>{data.question}</h4>
+          <select
+            onChange={(event) => this.handleChangeOfDropDown(data._id, event)}
+          >
+            <option value={data.c1}>{data.c1}</option>
+            <option value={data.c2}>{data.c2}</option>
+            <option value={data.c3}>{data.c3}</option>
+            <option value={data.c4}>{data.c4}</option>
+          </select>
+        </div>
+      );
+    } else if (type === "wr") {
+      return (
+        <div>
+          <h4>{data.question}</h4>
+          <form>
+            <p className="typeBelow">Type your answer below.</p>
+            <label>
+              <input
+                type="text"
+                name="name"
+                onChange={(event) =>
+                  this.handleWrittenResponse(data._id, event)
+                }
+              />
+            </label>
+          </form>
+          {/* <form>
+            <label for="writtenResponse">Type your answer below</label>
+            <br />
+            <input type="text" id="writtenResponse" name="writtenResponse" />
+            <br />
+          </form> */}
+        </div>
+      );
+    }
+  }
   render() {
+    var theQuestion1 = this.displayQuestion("tf", this.state.questions[0]);
+    var theQuestion2 = this.displayQuestion("tf", this.state.questions[1]);
+    var theQuestion3 = this.displayQuestion("mc", this.state.questions[2]);
+    var theQuestion4 = this.displayQuestion("mc", this.state.questions[3]);
+    var theQuestion5 = this.displayQuestion("wr", this.state.questions[4]);
+
     return (
       <div className="content">
         <button className="button" onClick={() => this.getQuestion()}>
           Start when you are ready!
         </button>
+        <div className="questions">
+          {theQuestion1}
+          <br></br>
+          <br></br>
+          <hr></hr>
+          {theQuestion2}
+          <br></br>
+          <br></br> <hr></hr>
+          {theQuestion3}
+          <br></br>
+          <br></br>
+          <hr></hr>
+          {theQuestion4}
+          <br></br>
+          <br></br>
+          <hr></hr>
+          {theQuestion5}
+          <p> {this.state.questions[0].userTfAnswer} </p>
+          <p>{this.state.userAnswer}</p>
+          <br></br>
+          <hr></hr>
+        </div>
 
-        <Question questionData={this.state.questions[0]} typeOfQuestion="tf" />
+        {/* <Question questionData={this.state.questions[0]} typeOfQuestion="tf" />
         <Question questionData={this.state.questions[1]} typeOfQuestion="mc" />
         <Question questionData={this.state.questions[2]} typeOfQuestion="mc" />
         <Question questionData={this.state.questions[3]} typeOfQuestion="dd" />
-        <Question questionData={this.state.questions[4]} typeOfQuestion="wr" />
+        <Question questionData={this.state.questions[4]} typeOfQuestion="wr" /> */}
+
+        <div>
+          <button className="submitButton" type="submit">
+            Submit
+          </button>
+        </div>
       </div>
     );
   }
